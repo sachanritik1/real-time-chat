a3d845bf028fb0988e3b132a2490ee11c6f92c6c
Ritik Sachan <sachanritik1@gmail.com>
2024-10-17 17:15:51 +0530

    Refactor: Update .gitignore and package dependencies
    Update .gitignore to include .env file and update package dependencies to add dotenv support. This allows loading environment variables from a .env file. Also, update the Redis client configuration to use the REDIS_URL environment variable if available, or fallback to the default localhost configuration.

--- chat-service/src/redis.ts
+++ chat-service/src/redis.ts
@@ -1,11 +1,20 @@
 import { createClient, RedisClientType } from "redis";
+import dotenv from "dotenv";
+import path from "path";
+
+// Load environment variables from .env file
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 let redisClient: RedisClientType;
 
 export function getRedisClient() {
   if (redisClient) return redisClient;
 
-  redisClient = createClient();
+  console.log("redis url", process.env.REDIS_URL);
+
+  redisClient = createClient({
+    url: process.env.REDIS_URL || "redis://localhost:6379",
+  });
   (async () => {
     redisClient.on("error", (err: any) =>
       console.log("Redis Client Error", err)
